6035710de6865d4d1e8aa431fe12c645f374de38
Invalid <zappeln@gmail.com>
2024-07-30 18:59:36 -0500

    added more test, board methods, and readme

--- src/main/java/net/shawnklein/tic_tac_toe/Board.java
+++ src/main/java/net/shawnklein/tic_tac_toe/Board.java
@@ -40,6 +40,17 @@ public class Board {
     return true;
   }
 
+  public String getWinner() {
+    int indices[][] = {{0,1,2},{3,4,5},{6,7,8},
+                       {0,3,6},{1,4,7},{2,5,8},
+                       {0,4,8},{2,4,6}};
+
+    for (int[] i : indices) {
+      
+    }
+    return " ";
+  }
+
   // possibly change these to return common character
   public boolean isColMatch() {
     boolean a = !board[0].equals(" ") && board[0].equals(board[3]) && board[0].equals(board[6]);
